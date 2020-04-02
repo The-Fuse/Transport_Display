8033109f4112ef8eda980ed90705e941b1fe9dc9
rohit141002 <rohitkumaryadav141002@gmail.com>
2020-04-03 01:00:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/transportdisplay/MapsActivity.kt
@@ -0,0 +1,103 @@
+package com.example.transportdisplay
+
+import android.os.Bundle;
+import android.util.Log;
+import androidx.fragment.app.FragmentActivity
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+
+class MapsActivity : FragmentActivity(), OnMapReadyCallback {
+    private val mMarkers = HashMap<String, Marker>()
+    private var mMap: GoogleMap? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        // Authenticate with Firebase when the Google map is loaded
+        mMap = googleMap
+        mMap!!.setMaxZoomPreference(16f)
+        loginToFirebase()
+    }
+
+    private fun loginToFirebase() {
+        val email = getString(R.string.firebase_email)
+        val password = getString(R.string.firebase_password)
+        // Authenticate with Firebase and subscribe to updates
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                subscribeToUpdates()
+                Log.d(TAG, "firebase auth success")
+            } else {
+                Log.d(TAG, "firebase auth failed")
+            }
+        }
+    }
+    private fun subscribeToUpdates() {
+        val ref = FirebaseDatabase.getInstance().getReference(getString(R.string.firebase_path))
+        ref.addChildEventListener(object : ChildEventListener {
+            override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                setMarker(dataSnapshot)
+            }
+
+            override fun onChildChanged(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                setMarker(dataSnapshot)
+            }
+
+            override fun onChildMoved(dataSnapshot: DataSnapshot, previousChildName: String?) {}
+            override fun onChildRemoved(dataSnapshot: DataSnapshot) {}
+            override fun onCancelled(error: DatabaseError) {
+                Log.d(TAG, "Failed to read value.", error.toException())
+            }
+        })
+    }
+
+    private fun setMarker(dataSnapshot: DataSnapshot) {
+        // When a location update is received, put or update
+        // its value in mMarkers, which contains all the markers
+        // for locations received, so that we can build the
+        // boundaries required to show them all on the map at once
+        val key = dataSnapshot.key
+        val value = dataSnapshot.value as HashMap<String, Any>?
+        val lat = value!!["latitude"].toString().toDouble()
+        val lng = value["longitude"].toString().toDouble()
+        val location = LatLng(lat, lng)
+        if (!mMarkers.containsKey(key)) {
+            mMarkers[key!!] = mMap!!.addMarker(MarkerOptions().title(key).position(location))
+        } else {
+            mMarkers[key]!!.position = location
+        }
+        val builder = LatLngBounds.Builder()
+        for (marker in mMarkers.values) {
+            builder.include(marker.position)
+        }
+        mMap!!.animateCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), 300))
+    }
+    companion object {
+        private val TAG = MapsActivity::class.java.simpleName
+    }
+}
